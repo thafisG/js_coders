ea110faa2d28b48117ec5f4e4f1c771ec67526fd
Thais Guedes <144493551+thafisG@users.noreply.github.com>
2024-06-22 14:48:28 -0300

    Create desafio_02.js

---
+++ desafios/desafio_02.js
@@ -0,0 +1,9 @@
+// Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+let fimDeSemana = prompt("Qual é o dia da semana?")
+
+if (fimDeSemana == "sabado" || fimDeSemana == "Domingo") {
+    console.log("Bom fim de Semana!")
+} else {
+    console.log("ixi, meio da semana é de lascar")
+}
