b4fabf3be1367a00c22c704d3dde018ac78e9f1f
Thais Guedes <144493551+thafisG@users.noreply.github.com>
2024-07-06 15:29:16 -0300

    Create desafios_funcao_listas.js

---
+++ desafios/desafios_funcao_listas.js
@@ -0,0 +1,8 @@
+# Crie uma lista vazia, com o nome listaGenerica.
+listaGenerica = [];
+# Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+# Adicione à lista linguagensDeProgramacao os seguintes elementos: Java, Ruby e GoLang.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+# Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+console.log(linguagensDeProgramacao[1]);
