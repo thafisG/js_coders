3e9ddc5f450197b344a796be5dc3929326916efd
Thais Guedes <144493551+thafisG@users.noreply.github.com>
2024-07-30 17:23:09 -0300

    Create tituloandparagrafo.js

---
+++ desafios/tituloandparagrafo.js
@@ -0,0 +1,3 @@
+<h1>ALURA - Mergulhe em Tecnologia!</h1>
+<p>Você vai estudar, praticar, discutir e se aprofundar em uma plataforma que respira tecnologia.</p>
+<p>Mergulhe com profundidade e navegue em outras áreas de Tecnologia. Profissional em T.</p>
